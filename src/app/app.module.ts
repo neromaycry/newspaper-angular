6cecb76ff61921ce186ec5590d25e7afd533d24c
unknown <gjwlg@163.com>
2018-03-05 22:18:45 +0800

    remove angular2-busy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 // import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
-import { BusyModule } from 'angular2-busy';
 // import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
@@ -20,7 +19,6 @@ import { NewsArticleModule } from './news-article';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    BusyModule,
     HttpClientModule,
     CoreModule,
     NewsListModule,
